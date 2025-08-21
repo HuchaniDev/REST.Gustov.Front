85c53e7243094a206194e3df35a823e4ea7a8f65
Daniel Huchani <161368230+HuchaniDev@users.noreply.github.com>
2025-08-21 05:16:12 -0400

    feat: show order ticked

--- src/app/features/main-layout/views/sales/ticked-show/ticked-show.component.ts
+++ src/app/features/main-layout/views/sales/ticked-show/ticked-show.component.ts
@@ -1,18 +1,23 @@
 import {Component, inject} from "@angular/core";
 import {DialogService} from "../../../../../shared/controls/dialog";
 import {SaleDetailTickedInterface} from "../../../models/sale/sale-detail-ticked.interface";
-import {JsonPipe} from "@angular/common";
+import {CurrencyPipe, JsonPipe, TitleCasePipe} from "@angular/common";
 
 @Component({
     selector: 'ticked-show',
     templateUrl: 'ticked-show.component.html',
     standalone: true,
     imports: [
-        JsonPipe
+        JsonPipe,
+        CurrencyPipe,
+        TitleCasePipe
     ]
 })
 export class TickedShowComponent {
     #dialogService=inject(DialogService)
-
     ticked = this.#dialogService.dialogConfig?.data as SaleDetailTickedInterface;
+
+    close(){
+        this.#dialogService.close();
+    }
 }
