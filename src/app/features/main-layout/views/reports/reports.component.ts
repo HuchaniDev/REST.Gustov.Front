5074b30816c8cbd3a8054bc86e7a76dbd67a7f5c
Daniel Huchani <161368230+HuchaniDev@users.noreply.github.com>
2025-09-09 08:34:40 -0400

    feat: view report

--- src/app/features/main-layout/views/reports/reports.component.ts
+++ src/app/features/main-layout/views/reports/reports.component.ts
@@ -1,12 +1,66 @@
-import {Component} from "@angular/core";
+import {Component, inject} from "@angular/core";
+import {InputDirective} from "../../../../shared/directives/input.directive";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {DailySalesReportDto} from "../../models/reports/repotrs.interface";
+import {CurrencyPipe, DatePipe} from "@angular/common";
+import {ReportService} from "../../services/report.service";
 
 @Component({
     selector: "app-reports",
     templateUrl:'reports.component.html',
     standalone:true,
-    imports:[]
+    imports: [
+        InputDirective,
+        ReactiveFormsModule,
+        FormsModule,
+        DatePipe,
+        CurrencyPipe
+    ]
 })
 export default class ReportsComponent {
+    #reportService=inject(ReportService)
+    dateSearch:string='';
 
+    report!:DailySalesReportDto;
+
+    getReport(){
+
+        if(this.dateSearch)
+            this.getReportByDate(this.dateSearch);
+        else
+            this.getTodayReport()
+
+    }
+
+    getTodayReport(){
+        this.#reportService.getTodayReport().subscribe({
+            next: value => {
+                this.report=value.data;
+                console.log(this.report);
+
+            },
+            error: err => {
+                console.log(err)
+            }
+        })
+    }
+    getReportByDate(date:string){
+        this.#reportService.getReport(date).subscribe({
+            next: value => {
+                this.report=value.data;
+                console.log(this.report);
+            },
+            error: err => {
+                console.log(err)
+            }
+        })
+    }
+
+
+
+    #getOnlyDate(input: string | Date): string {
+        const date = new Date(input);
+        return date.toISOString().split('T')[0];
+    }
 
 }
