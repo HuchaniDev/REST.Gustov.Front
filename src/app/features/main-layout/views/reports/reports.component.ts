7c907fcc8b68efc0a6e3a00fa64503a323575983
Daniel Huchani <161368230+HuchaniDev@users.noreply.github.com>
2025-09-09 07:34:56 -0400

    feat: add reports route

---
+++ src/app/features/main-layout/views/reports/reports.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "app-reports",
+    templateUrl:'reports.component.html',
+    standalone:true,
+    imports:[]
+})
+export default class ReportsComponent {
+
+
+}
